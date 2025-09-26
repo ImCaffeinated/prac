8abcfdc01bfebe4b46cbbc60008c334d3c8f3434
ImCaffeinated <avengers1423@live.com>
2025-09-26 13:36:58 +0000

    Completed Magic 8 ball with 10 different answers.

--- randomNum.c
+++ randomNum.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 
 int randomNumberGen() {
-
-int randomNumber = (rand() % 3) + 1;
+srand(time(NULL));
+int randomNumber = (rand() % 10) + 1;
 
 if (randomNumber == 1) {
     printf("Yes\n");
@@ -15,6 +16,27 @@ if (randomNumber == 1) {
     }
     else if (randomNumber == 3){
         printf("Maybe\n");
+    }
+    else if (randomNumber == 4){
+        printf("Ask again later\n");
+    }
+    else if (randomNumber == 5){
+        printf("Definitely\n");
+    }
+    else if (randomNumber == 6){
+        printf("Absolutely not\n");
+    }
+    else if (randomNumber == 7){
+        printf("It is certain\n");
+    }
+    else if (randomNumber == 8){
+        printf("Very doubtful\n");
+    }
+    else if (randomNumber == 9){
+        printf("Without a doubt\n");
+    }
+    else if (randomNumber == 10){
+        printf("Concentrate and ask again\n");
     }
 else {
     printf("Error");
